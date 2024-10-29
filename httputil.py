732558d5e7b224114958e61dd40ed27ab56089ec
tom <thomastedrow@fastmail.com>
2024-10-29 12:59:14 -0500

    fix format/lint issues

--- httputil.py
+++ httputil.py
@@ -2,20 +2,22 @@ import urllib.request
 import urllib.parse
 import json
 
-def get(url, params=None, headers=None): 
-    if params != None:
-        urlEncodedParams =  urllib.parse.urlencode(params)
+
+def get(url, params=None, headers=None):
+    if params is not None:
+        urlEncodedParams = urllib.parse.urlencode(params)
         url = url + urlEncodedParams
     req = urllib.request.Request(url)
-    if headers != None:
+    if headers is not None:
         for name, value in headers.items():
             req.add_header(name, value)
     response = urllib.request.urlopen(req)
     return response
 
+
 def post(url, data, headers=None):
     req = urllib.request.Request(url, method="POST")
-    if headers != None:
+    if headers is not None:
         for name, value in headers.items():
             req.add_header(name, value)
     data = json.dumps(data)
--- sleeper_cli.py
+++ sleeper_cli.py
@@ -2,9 +2,10 @@ import httputil
 import json
 import discord_client
 from team import Team
-from matchup import Matchup 
+from matchup import Matchup
 from power_rankings import PowerRankings
-from format import get_standings_text, get_matchups_text, get_power_rankings_text, get_trophies_text
+from format import (get_standings_text, get_matchups_text,
+                    get_power_rankings_text, get_trophies_text)
 
 urls = {
     "get_users": "https://api.sleeper.app/v1/league/{}/users",
@@ -12,24 +13,27 @@ urls = {
     "get_matchups": "https://api.sleeper.app/v1/league/{}/matchups/{}"
 }
 
+
 def get_teams(league_id):
     url = urls['get_users'].format(league_id)
     response = httputil.get(url)
     parsed = json.loads(response.read().decode('utf-8'))
     teams = []
     for team in parsed:
-        teams.append(Team(team['display_name'],team['user_id']))
+        teams.append(Team(team['display_name'], team['user_id']))
     return teams
 
-def get_rosters_keyed_by_owner_id(league_id): 
+
+def get_rosters_keyed_by_owner_id(league_id):
     url = urls['get_roster'].format(league_id)
     response = httputil.get(url)
     parsed = json.loads(response.read().decode('utf-8'))
-    rosters_keyed_by_owner_id = {} 
+    rosters_keyed_by_owner_id = {}
     for roster in parsed:
         owner_id = roster['owner_id']
         rosters_keyed_by_owner_id[owner_id] = roster
     return rosters_keyed_by_owner_id
+
 
 def get_teams_with_roster_data(teams, roster_keyed_by_owner_id):
     for team in teams:
@@ -42,11 +46,13 @@ def get_teams_with_roster_data(teams, roster_keyed_by_owner_id):
         team.add_roster_data(roster['roster_id'], wins, losses, ties, pf, pa)
     return teams
 
+
 def get_roster_ids_to_team_name(teams):
     roster_ids_to_team_name = {}
     for team in teams:
         roster_ids_to_team_name[team.roster_id] = team.display_name
-    return roster_ids_to_team_name  
+    return roster_ids_to_team_name
+
 
 def get_matchups(league_id, number_of_weeks_played):
     matchups = []
@@ -58,15 +64,19 @@ def get_matchups(league_id, number_of_weeks_played):
         for matchup in parsed:
             matchup_id = matchup['matchup_id']
             if matchup_id in weekly_matchups:
-                weekly_matchups[matchup_id].add_opponent_data(matchup['roster_id'],matchup['points'])
+                weekly_matchups[matchup_id].add_opponent_data(
+                    matchup['roster_id'], matchup['points'])
             else:
-                weekly_matchups[matchup_id] = Matchup(matchup['roster_id'],matchup['points'],week)
+                weekly_matchups[matchup_id] = Matchup(
+                    matchup['roster_id'], matchup['points'], week)
         matchups.extend(weekly_matchups.values())
     sorted_matchups = sorted(matchups, key=lambda x: x.week, reverse=False)
     return sorted_matchups
 
+
 def get_matchups_by_week(matchups, week):
     return [matchup for matchup in matchups if matchup.week == week]
+
 
 def get_trophies(matchups, roster_ids_to_team_name):
     low_score = 9999
@@ -88,10 +98,10 @@ def get_trophies(matchups, roster_ids_to_team_name):
             high_score = winner_score
             high_team_name = roster_ids_to_team_name[winner_roster_id]
         if loser_score < low_score:
-            low_score = loser_score 
+            low_score = loser_score
             low_team_name = roster_ids_to_team_name[loser_roster_id]
         if score_difference < closest_score:
-            closest_score = score_difference 
+            closest_score = score_difference
             close_winner = roster_ids_to_team_name[winner_roster_id]
             close_loser = roster_ids_to_team_name[loser_roster_id]
         if score_difference > biggest_blowout:
@@ -99,7 +109,10 @@ def get_trophies(matchups, roster_ids_to_team_name):
             ownerer_team_name = roster_ids_to_team_name[winner_roster_id]
             blown_out_team_name = roster_ids_to_team_name[loser_roster_id]
 
-    return (low_team_name, low_score, high_team_name, high_score, close_winner, close_loser, closest_score, blown_out_team_name, ownerer_team_name, biggest_blowout) 
+    return (low_team_name, low_score, high_team_name, high_score, close_winner,
+            close_loser, closest_score, blown_out_team_name, ownerer_team_name,
+            biggest_blowout)
+
 
 def print_choices():
     print()
@@ -109,6 +122,7 @@ def print_choices():
     print('3 to post weekly matchups')
     print('4 to post weekly trophies')
     print('5 to send a custom message')
+
 
 if __name__ == '__main__':
     league_id = 0
@@ -120,7 +134,8 @@ if __name__ == '__main__':
         channel_id = tokens['channelId']
         league_id = tokens['sleeperLeagueId']
     number_of_weeks_played = int(input('enter number of weeks played:\n'))
-    print('gathering data for {} weeks of play\n'.format(str(number_of_weeks_played)))
+    print('gathering data for {} weeks of play\n'.format(
+        str(number_of_weeks_played)))
     teams = get_teams(league_id)
     rosters_keyed_by_owner_id = get_rosters_keyed_by_owner_id(league_id)
     teams = get_teams_with_roster_data(teams, rosters_keyed_by_owner_id)
@@ -129,7 +144,8 @@ if __name__ == '__main__':
     for week in range(1, number_of_weeks_played + 1):
         weekly_matchups = get_matchups_by_week(matchups, week)
         print(get_matchups_text(weekly_matchups, roster_ids_to_team_name, week))
-        print(get_trophies_text(get_trophies(weekly_matchups, roster_ids_to_team_name)))
+        print(get_trophies_text(get_trophies(
+            weekly_matchups, roster_ids_to_team_name)))
         print()
     power_rankings = PowerRankings(matchups, len(teams))
     scoreboard = power_rankings.get_scoreboard(0.5)
@@ -144,13 +160,16 @@ if __name__ == '__main__':
         if choice == '1':
             message = get_standings_text(teams)
         elif choice == '2':
-            message = get_power_rankings_text(scoreboard, teams, roster_ids_to_team_name)
+            message = get_power_rankings_text(
+                scoreboard, teams, roster_ids_to_team_name)
         elif choice == '3':
             week = int(input('enter week\n'))
-            message = get_matchups_text(weekly_matchups, roster_ids_to_team_name, week)
+            message = get_matchups_text(
+                weekly_matchups, roster_ids_to_team_name, week)
         elif choice == '4':
             week = int(input('enter week\n'))
-            message = get_trophies_text(get_trophies(weekly_matchups, roster_ids_to_team_name))
+            message = get_trophies_text(get_trophies(
+                weekly_matchups, roster_ids_to_team_name))
         elif choice == '5':
             message = input('enter your message\n')
         print()
